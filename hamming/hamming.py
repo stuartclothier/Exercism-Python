7311e70a57d4c109a90cd4a9671a086199dd925e
Stuart Clothier <stuartclothier@gmail.com>
2021-05-06 10:50:15 +0100

    formatting on hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,6 +1,5 @@
-def distance(strand_a, strand_b):
+def distance(strand_a: str, strand_b: str):
     if len(strand_a) == len(strand_b):
-        return sum(1 for a,b in zip(strand_a,strand_b) if a!=b)
+        return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
     else:
         raise ValueError('Strands have different lengths')
-        
