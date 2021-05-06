b0b0209cccfd8620de2e352aaf11012e5a5893da
Stuart Clothier <stuartclothier@gmail.com>
2021-05-06 13:46:08 +0100

    isbn final solution

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,9 +1,15 @@
+import string
+import random
+
+
 class Robot:
-    
+
     def __init__(self):
-        import string
-        import random
-        self.name = f"{''.join(random.choices(string.ascii_uppercase, k = 2))}{''.join(random.choices(string.digits,k=3))}"
-        
-    def reset
+        self.reset()
+
+    def reset(self):
+        self.name = f"{''.join(random.choices(string.ascii_uppercase,k = 2))}{''.join(random.choices(string.digits,k=3))}"
+
+
+print(Robot().name)
 print(Robot().name)
