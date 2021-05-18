a5ce0ecea01af07d28f24001cb852c887d02de38
Stuart Clothier <stuartclothier@gmail.com>
2021-05-18 19:15:15 +0100

    Minor formatting on allergies.py

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -3,14 +3,8 @@ from math import log
 
 class Allergies:
 
-    all_allergies = ['eggs',
-                     'peanuts',
-                     'shellfish',
-                     'strawberries',
-                     'tomatoes',
-                     'chocolate',
-                     'pollen',
-                     'cats']
+    all_allergies = ['eggs', 'peanuts', 'shellfish', 'strawberries',
+                     'tomatoes', 'chocolate', 'pollen', 'cats']
 
     def __init__(self, score: int) -> None:
         self.allergy_list = [self.all_allergies[i]
