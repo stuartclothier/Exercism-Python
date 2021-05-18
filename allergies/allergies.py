9bd081e0a677b56052e23c1463b96a72b5cb7b4a
Stuart Clothier <stuartclothier@gmail.com>
2021-05-18 19:37:37 +0100

    update allergies.py

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -7,8 +7,13 @@ class Allergies:
                      'tomatoes', 'chocolate', 'pollen', 'cats']
 
     def __init__(self, score: int) -> None:
+        # n determines the highest indexed allergy possible
+        n = (int(log(score)/log(2)) if score else 0) \
+            if score < 256 else 7
+
+        # Create allergy list for score
         self.allergy_list = [self.all_allergies[i]
-                             for i in range(8) if score//2**i % 2]
+                             for i in range(n+1) if score//2**i % 2]
 
     def allergic_to(self, item: str) -> bool:
         return item in self.allergy_list
