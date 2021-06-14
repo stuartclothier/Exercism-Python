d8acf753a3a870eff486284841582856159528d3
Stuart Clothier <stuartclothier@gmail.com>
2021-06-14 23:15:09 +0100

    word_count sol

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,18 @@
+import re
+
+# using findall
 def count_words(sentence):
-    pass
+
+    pattern = re.compile(r"[a-z0-9']+")
+
+    word_list = [word.strip("'") for word in pattern.findall(sentence.lower())]
+    return {i: word_list.count(i) for i in word_list}
+
+
+# using split
+def count_words(sentence):
+
+    pattern = re.compile(r"[^a-z0-9']|_")
+    word_list = [word.strip("'")
+                 for word in pattern.split(sentence.lower()) if word]
+    return {i: word_list.count(i) for i in word_list}
