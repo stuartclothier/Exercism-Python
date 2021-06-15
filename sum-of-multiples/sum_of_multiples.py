c0cdf6f3b5c3cec3f8f6f2bdfac0eb839953da19
Stuart Clothier <stuartclothier@gmail.com>
2021-06-15 12:05:11 +0100

    sum_of_multipls sol

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,10 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit: int, multiples: list[int]) -> int:
+
+    return sum(
+        {
+            multiple
+            for factor in multiples
+            if factor
+            for multiple in range(factor, limit, factor)
+        }
+    )
