05835674f7c5d24bab82b236ecd898f5bdd86624
Stuart Clothier <stuartclothier@gmail.com>
2021-06-18 20:04:11 +0100

    grains solution

---
+++ grains/grains.py
@@ -0,0 +1,9 @@
+def square(number):
+    if not 1 <= number <= 64:
+        raise ValueError("Number must be between 1 and 64.")
+    
+    return 2**(number-1)
+
+
+def total():
+    return 2 * square(64) - 1
