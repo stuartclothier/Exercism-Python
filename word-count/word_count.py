5a891068ea3a38123753bd2f5ca3ed08f052c89d
Stuart Clothier <stuartclothier@gmail.com>
2021-08-02 16:02:50 +0100

    word_count edit

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,18 +1,14 @@
 import re
 
-# using findall
-def count_words(sentence):
 
-    pattern = re.compile(r"[a-z0-9']+")
-
-    word_list = [word.strip("'") for word in pattern.findall(sentence.lower())]
-    return {i: word_list.count(i) for i in word_list}
-
-
-# using split
 def count_words(sentence):
 
     pattern = re.compile(r"[^a-z0-9']|_")
-    word_list = [word.strip("'")
-                 for word in pattern.split(sentence.lower()) if word]
-    return {i: word_list.count(i) for i in word_list}
+    word_list = [
+        word.strip("'") for word in pattern.split(sentence.lower()) if word
+    ]
+
+    count = dict()
+    for i in word_list:
+        count[i] = count.get(i, 0) + 1
+    return count
