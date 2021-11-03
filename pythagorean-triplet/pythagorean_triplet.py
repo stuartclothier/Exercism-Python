ae8ce54d16c977f2105e54f32b5304d3d329303f
Stuart Clothier <stuartclothier@gmail.com>
2021-11-03 18:20:43 +0000

    pythangorean_triplet sol

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,2 +1,9 @@
-def triplets_with_sum(number):
-    pass
+def triplets_with_sum(n):
+     
+    trip = []
+    for a in range(n//3-1, 2, -1):
+        b = n*(n-2*a) // (2*(n-a))
+        c = n - a - b
+        if a<b and a*a + b*b == c*c:
+            trip.append([a,b,c]); 
+    return trip
