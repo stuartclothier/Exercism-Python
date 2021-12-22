7eea510c466e53abfd3e8380909ae3d2450c5340
Stuart Clothier <stuartclothier@gmail.com>
2021-12-22 14:52:03 +1100

    clock soln

---
+++ clock/clock.py
@@ -0,0 +1,17 @@
+class Clock:
+    def __init__(self, hour, minute):
+        self.hour = (hour + minute // 60) % 24
+        self.minute = minute % 60
+        return None
+
+    def __repr__(self):
+        return "{:0>2}:{:0>2}".format(self.hour, self.minute)
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def __add__(self, minutes):
+        return Clock(self.hour, self.minute + minutes)
+
+    def __sub__(self, minutes):
+        return Clock(self.hour, self.minute - minutes)
