56170d385bb90bf048d467e3fc9d0dd3cdf24048
Stuart Clothier <stuartclothier@gmail.com>
2022-01-30 22:55:02 +1100

    triangle update

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,14 +1,20 @@
-def is_triangle(sides: list[int]) -> bool:
-    return all(ele > 0 for ele in sides) and max(sides) <= sum(sides) / 2
+from typing import Callable
 
 
+def is_triangle(sides: list[int]) -> Callable[..., bool]:
+    return lambda s: all(s) and 2 * max(s) < sum(s) and sides(s)
+
+
+@is_triangle
 def equilateral(sides: list[int]) -> bool:
-    return len(set(sides)) == 1 and is_triangle(sides)
+    return len(set(sides)) == 1
 
 
+@is_triangle
 def isosceles(sides: list[int]) -> bool:
-    return len(set(sides)) <= 2 and is_triangle(sides)
+    return len(set(sides)) <= 2
 
 
+@is_triangle
 def scalene(sides: list[int]) -> bool:
-    return len(set(sides)) == 3 and is_triangle(sides)
+    return len(set(sides)) == 3
