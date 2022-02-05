e53b059bbd0fe7fc4ccdc01b1db8055a9e3228b1
Stuart Clothier <stuartclothier@gmail.com>
2022-02-05 17:14:15 +1100

    brackets soln

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,2 +1,25 @@
 def is_paired(input_string):
-    pass
+    """
+    Given a string, return True if all brackets are paired
+    in the correct order, otherwise return False
+
+    :param input_string: The string to be tested
+    :return: True or False
+    """
+
+    brackets = {")": "(", "}": "{", "]": "["}
+
+    bracket_list = [each for each in input_string if each in "(){}[]"]
+
+    state = []
+
+    for each in bracket_list:
+        if each in brackets:
+            if state and brackets[each] == state[-1]:
+                state = state[:-1]
+                continue
+            else:
+                return False
+        state.append(each)
+
+    return not state
