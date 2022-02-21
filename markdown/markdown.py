493cc3419966b83cf4da4cd129d2958dac0fe19c
Stuart Clothier <stuartclothier@gmail.com>
2022-02-21 21:15:58 +1100

    markdown update

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -1,6 +1,9 @@
 import re
 
 
+############### ADD SIGNIFICANTLY CLEARER COMMENTS
+
+
 def parse(markdown):
 
     # Break lines
@@ -39,93 +42,20 @@ def parse(markdown):
         # Format bold and italics
         formats = {
             "strong": r"(\*\*(.*)\*\*)|(__(.*)__)",
-            "em": r"((?<!\*)\*(?!\*)(.*)\*)|_(.*)_" # Possibly add look behind
+            "em": r"((?<!\*)\*(?!\*)(.*)\*)|_(.*)_",  # Possibly add look behind
         }
-        
-        for el in formats.values():
-            if re.search(el, i):
-                m1 = re.search(el, i)
-                i = m1.string[0:m1.start()]+"<strong>"+m1.group(1)+"</strong>"+m1.string[m1.end():]
-        
+
+        for element, reg in formats.items():
+            if re.search(reg, i):
+                m1 = re.search(reg, i)
+
+                i = f"""{m1.string[0 : m1.start()]}<{element}>{m1.groups()[-1]}</{element}>{m1.string[m1.end() :]}"""
+
+        # Think this adds outer tags, e.g. heading or paragraph
         i = f"<{element}>{i}</{element}>"
         res += i
 
     return res
 
-print(parse("__This will be bold__"))
 
-    #     if m:
-    #         if not in_list:
-    #             in_list = True
-    #             is_bold = False
-    #             is_italic = False
-    #             curr = m.group(1)
-    #             m1 = re.match("(.*)__(.*)__(.*)", curr)
-    #             if m1:
-    #                 curr = (
-    #                     m1.group(1)
-    #                     + "<strong>"
-    #                     + m1.group(2)
-    #                     + "</strong>"
-    #                     + m1.group(3)
-    #                 )
-    #                 is_bold = True
-    #             m1 = re.match("(.*)_(.*)_(.*)", curr)
-    #             if m1:
-    #                 curr = (
-    #                     m1.group(1)
-    #                     + "<em>"
-    #                     + m1.group(2)
-    #                     + "</em>"
-    #                     + m1.group(3)
-    #                 )
-    #                 is_italic = True
-    #             i = "<ul><li>" + curr + "</li>"
-    #         else:
-    #             is_bold = False
-    #             is_italic = False
-    #             curr = m.group(1)
-    #             m1 = re.match("(.*)__(.*)__(.*)", curr)
-    #             if m1:
-    #                 is_bold = True
-    #             m1 = re.match("(.*)_(.*)_(.*)", curr)
-    #             if m1:
-    #                 is_italic = True
-    #             if is_bold:
-    #                 curr = (
-    #                     m1.group(1)
-    #                     + "<strong>"
-    #                     + m1.group(2)
-    #                     + "</strong>"
-    #                     + m1.group(3)
-    #                 )
-    #             if is_italic:
-    #                 curr = (
-    #                     m1.group(1)
-    #                     + "<em>"
-    #                     + m1.group(2)
-    #                     + "</em>"
-    #                     + m1.group(3)
-    #                 )
-    #             i = "<li>" + curr + "</li>"
-    #     else:
-    #         if in_list:
-    #             in_list_append = True
-    #             in_list = False
-
-    #     m = re.match("<h|<ul|<p|<li", i)
-    #     if not m:
-    #         i = "<p>" + i + "</p>"
-    #     m = re.match("(.*)__(.*)__(.*)", i)
-    #     if m:
-    #         i = m.group(1) + "<strong>" + m.group(2) +
-    # "</strong>" + m.group(3)
-    #     m = re.match("(.*)_(.*)_(.*)", i)
-    #     if m:
-    #         i = m.group(1) + "<em>" + m.group(2) + "</em>" + m.group(3)
-    #     if in_list_append:
-    #         i = "</ul>" + i
-    #         in_list_append = False
-    #     res += i
-    # if in_list:
-    #     res += "</ul>"
+print(parse("__How does _This will be bold___"))
